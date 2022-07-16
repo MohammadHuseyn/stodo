286c85139371b5889a92238c4884fc41bbc170b3
Mohammad Huseyn <mohammadhuseynamini@gmail.com>
2022-07-16 23:23:42 +0430

    adding menu buttons, drawer bug fixed and editing card shapes

--- lib/dart files/HomePage.dart
+++ lib/dart files/HomePage.dart
@@ -13,56 +13,157 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawerEdgeDragWidth: MediaQuery.of(context).size.width,
         drawer: Drawer(
           width: MediaQuery.of(context).size.width * 0.8,
-          child: Column(
-            children: [
-              Container(
-                decoration: new BoxDecoration(boxShadow: [
-                  new BoxShadow(
-                      color: Colors.indigo, blurRadius: 100, spreadRadius: 0.01)
-                ]),
-                child: Card(
-                  margin: EdgeInsets.all(25),
-                  child: Row(
-                    // crossAxisAlignment: CrossAxisAlignment.start,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  decoration: new BoxDecoration(boxShadow: [
+                    new BoxShadow(
+                        color: Colors.indigo, blurRadius: 500, spreadRadius: 0.01)
+                  ]),
+                  child: Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: CircleAvatar(
-                          minRadius: MediaQuery.of(context).size.width * 0.16,
-                          backgroundImage: const AssetImage("avatar.png"),
+                      Card(
+                        margin: EdgeInsets.all(25),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Row(
+                          // crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: CircleAvatar(
+                                minRadius:
+                                MediaQuery.of(context).size.width * 0.16,
+                                backgroundImage: const AssetImage("avatar.png"),
+                              ),
+                            ),
+                            Container(
+                              // height: 100,
+                              width: 150,
+                              child: ListTile(
+                                trailing: IconButton(
+                                  icon: Icon(Icons.edit_rounded),
+                                  onPressed: (){},
+                                ),
+                                title: Text("Name",style: TextStyle(fontSize: 24),),
+                                subtitle: Text("username",style: TextStyle(fontSize: 14)),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "Name",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(fontSize: 25),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          const Text(
-                            "username",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ],
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        margin: EdgeInsets.only(left: 20, right: 20),
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              leading: Icon(
+                                Icons.edit_rounded,
+                                color: Colors.black,
+                              ),
+                              title: Text("Edit profile"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.playlist_add,
+                                color: Colors.black,
+                              ),
+                              title: Text("New list"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.list,
+                                color: Colors.black,
+                              ),
+                              title: Text("All lists"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.group,
+                                color: Colors.black,
+                              ),
+                              title: Text("Friends"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.bookmark,
+                                color: Colors.black,
+                              ),
+                              title: Text("Highlited"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.settings,
+                                color: Colors.black,
+                              ),
+                              title: Text("Setting"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                          ],
+                        ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 130,left: 10),
-                        child: IconButton(
-                            onPressed: () {}, icon: const Icon(Icons.edit)),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        margin: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              leading: Icon(
+                                Icons.person_add,
+                                color: Colors.black,
+                              ),
+                              hoverColor: Colors.orange,
+                              title: Text("Invite friends"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.phone,
+                                color: Colors.black,
+                              ),
+                              hoverColor: Colors.green,
+                              title: Text("Contact us"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                            ListTile(
+                              hoverColor: Colors.red,
+                              leading: Icon(
+                                Icons.logout,
+                                color: Colors.black,
+                              ),
+                              title: Text("Log out"),
+                              trailing: Icon(Icons.keyboard_arrow_right),
+                              onTap: () {},
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          )
         ),
         appBar: AppBar(
           title: Text(widget.title),
