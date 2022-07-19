87f5d668e2dbb7d19377586193d87912c7cef0d7
Mohammad Huseyn <mohammadhuseynamini@gmail.com>
2022-07-20 00:55:52 +0430

    adding task lists on page main body

--- lib/dart files/HomePage.dart
+++ lib/dart files/HomePage.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../classes/TaskList.dart';
+import '../classes/User.dart';
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage(
+      {Key? key,
+      required this.title,
+      required this.mainUser,
+      required this.taskLists})
+      : super(key: key);
 
   final String title;
+  final User mainUser;
+  List<TaskList> taskLists;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -15,159 +25,204 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         drawerEdgeDragWidth: MediaQuery.of(context).size.width,
         drawer: Drawer(
-          width: MediaQuery.of(context).size.width * 0.8,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  decoration: new BoxDecoration(boxShadow: [
-                    new BoxShadow(
-                        color: Colors.indigo, blurRadius: 500, spreadRadius: 0.01)
-                  ]),
-                  child: Column(
-                    children: [
-                      Card(
-                        margin: EdgeInsets.all(25),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Row(
-                          // crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(20),
-                              child: CircleAvatar(
-                                minRadius:
-                                MediaQuery.of(context).size.width * 0.16,
-                                backgroundImage: const AssetImage("avatar.png"),
-                              ),
-                            ),
-                            Container(
-                              // height: 100,
-                              width: 150,
-                              child: ListTile(
-                                trailing: IconButton(
-                                  icon: Icon(Icons.edit_rounded),
-                                  onPressed: (){},
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(boxShadow: [
+                      BoxShadow(
+                          color: Colors.indigo,
+                          blurRadius: 500,
+                          spreadRadius: 0.01)
+                    ]),
+                    child: Column(
+                      children: [
+                        Card(
+                          margin: const EdgeInsets.all(25),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          child: Row(
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: CircleAvatar(
+                                  minRadius:
+                                      MediaQuery.of(context).size.width * 0.16,
+                                  backgroundImage:
+                                      const AssetImage("avatar.png"),
                                 ),
-                                title: Text("Name",style: TextStyle(fontSize: 24),),
-                                subtitle: Text("username",style: TextStyle(fontSize: 14)),
                               ),
-                            ),
-                          ],
+                              SizedBox(
+                                // height: 100,
+                                width: 150,
+                                child: ListTile(
+                                  trailing: IconButton(
+                                    icon: const Icon(Icons.edit_rounded),
+                                    onPressed: () {},
+                                  ),
+                                  title: const Text(
+                                    "Name",
+                                    style: TextStyle(fontSize: 24),
+                                  ),
+                                  subtitle: const Text("username",
+                                      style: TextStyle(fontSize: 14)),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        margin: EdgeInsets.only(left: 20, right: 20),
-                        child: Column(
-                          children: <Widget>[
-                            ListTile(
-                              leading: Icon(
-                                Icons.edit_rounded,
-                                color: Colors.black,
+                        Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          margin: const EdgeInsets.only(left: 20, right: 20),
+                          child: Column(
+                            children: <Widget>[
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.edit_rounded,
+                                  color: Colors.black,
+                                ),
+                                title: const Text("Edit profile"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              title: Text("Edit profile"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                            ListTile(
-                              leading: Icon(
-                                Icons.playlist_add,
-                                color: Colors.black,
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.playlist_add,
+                                  color: Colors.black,
+                                ),
+                                title: const Text("New list"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              title: Text("New list"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                            ListTile(
-                              leading: Icon(
-                                Icons.list,
-                                color: Colors.black,
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.list,
+                                  color: Colors.black,
+                                ),
+                                title: const Text("All lists"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              title: Text("All lists"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                            ListTile(
-                              leading: Icon(
-                                Icons.group,
-                                color: Colors.black,
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.group,
+                                  color: Colors.black,
+                                ),
+                                title: const Text("Friends"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              title: Text("Friends"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                            ListTile(
-                              leading: Icon(
-                                Icons.bookmark,
-                                color: Colors.black,
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.bookmark,
+                                  color: Colors.black,
+                                ),
+                                title: const Text("Highlighted"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              title: Text("Highlited"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                            ListTile(
-                              leading: Icon(
-                                Icons.settings,
-                                color: Colors.black,
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.settings,
+                                  color: Colors.black,
+                                ),
+                                title: const Text("Setting"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              title: Text("Setting"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        margin: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-                        child: Column(
-                          children: <Widget>[
-                            ListTile(
-                              leading: Icon(
-                                Icons.person_add,
-                                color: Colors.black,
+                        Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 20),
+                          child: Column(
+                            children: <Widget>[
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.person_add,
+                                  color: Colors.black,
+                                ),
+                                hoverColor: Colors.orange,
+                                title: const Text("Invite friends"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              hoverColor: Colors.orange,
-                              title: Text("Invite friends"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                            ListTile(
-                              leading: Icon(
-                                Icons.phone,
-                                color: Colors.black,
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.phone,
+                                  color: Colors.black,
+                                ),
+                                hoverColor: Colors.green,
+                                title: const Text("Contact us"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              hoverColor: Colors.green,
-                              title: Text("Contact us"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                            ListTile(
-                              hoverColor: Colors.red,
-                              leading: Icon(
-                                Icons.logout,
-                                color: Colors.black,
+                              ListTile(
+                                hoverColor: Colors.red,
+                                leading: const Icon(
+                                  Icons.logout,
+                                  color: Colors.black,
+                                ),
+                                title: const Text("Log out"),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right),
+                                onTap: () {},
                               ),
-                              title: Text("Log out"),
-                              trailing: Icon(Icons.keyboard_arrow_right),
-                              onTap: () {},
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ),
+                ],
+              ),
+            )),
         appBar: AppBar(
           title: Text(widget.title),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(onPressed: () {}, icon: Icon(Icons.search_rounded))
+          ],
         ),
-        body: Container());
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          child: ListView.builder(
+            itemCount: widget.taskLists.length,
+            itemBuilder: (context,index){
+              return GestureDetector(
+                onTap: (){},
+                child: Card(
+                  margin: EdgeInsets.only(left: 15,right: 15,top: 10),
+                  child: ListTile(
+                    title: Text(widget.taskLists.elementAt(index).name,style: TextStyle(fontSize: 18),),
+                    subtitle: Text(widget.taskLists.elementAt(index).description),
+                    trailing: Wrap(
+                      spacing: 5,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: <Widget>[
+                        Text(widget.taskLists.elementAt(index).users.length.toString(),style: TextStyle(fontSize: 18),),
+                        Icon(Icons.people,size: 30,)
+                      ],
+                    )
+                  ),
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:stodo/classes/Task.dart';
+import 'package:stodo/classes/TaskList.dart';
+import 'package:stodo/classes/User.dart';
 
 import 'dart files/HomePage.dart';
 
@@ -12,12 +15,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    User mainUser = new User(firstname: "Mohammad Huseyn", lastname: "Amini", username: "mohammadhuseyn");
+    mainUser.setPass("password", "password");
+    Task task1 = new Task(title: "Task1", description: "desc1", owner: mainUser);
+    Task task2 = new Task(title: "Task2", description: "desc2", owner: mainUser);
+    Task task3 = new Task(title: "Task3", description: "desc3", owner: mainUser);
+    TaskList taskList1 = new TaskList(name: "Task List 1", AD: "CE", university: "Shahid Beheshti University",description: "description of taskList", creator: mainUser);
+    taskList1.tasks.add(task1);
+    taskList1.tasks.add(task2);
+    taskList1.tasks.add(task3);
+    Task task4 = new Task(title: "Task4", description: "desc4", owner: mainUser);
+    Task task5 = new Task(title: "Task5", description: "desc5", owner: mainUser);
+    Task task6 = new Task(title: "Task6", description: "desc6", owner: mainUser);
+    TaskList taskList2 = new TaskList(name: "Task List 2", AD: "CE", university: "Shahid Beheshti University",description: "description of taskList", creator: mainUser);
+    taskList2.tasks.add(task4);
+    taskList2.tasks.add(task5);
+    taskList2.tasks.add(task6);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'stodo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'SToDo'),
+      home: MyHomePage(title: 'Stodo',mainUser: mainUser,taskLists: [taskList1,taskList2]),
     );
   }
 }
